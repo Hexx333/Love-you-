23108eea218f291bff564a07fd0aa39c9ec4a04e
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-04-13 16:11:46 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,28 +1,5 @@
-<title>
-    "JebJep"
-    </title>
-    <head>
-let number = Math.floor(Math.random(123) = 101);
-    </head>
-    function play(\:, very,  She'three) {
-        var $ = function (id) {
-            $('vids').onclick = function (e) {
-                var src, id;
-                e = e || window.event;
-                src = e.target || e.srcElement;
-                if (src.nodeName !== "A") {
-                return;
-                }
-                if (typeof e.preventDefault === "function") {
-                e.preventDefault();
-                }
-                e.returnValue = false;
-                id = src.href.split('--')[1];
-                if (src.className === "play")
-                const element = document.getElementById(id);
- {
-            return document.getElementById(id);
-        let userNumber = window.parent.document.getElementById("userNumber").value; MessageChannel;
+
+
         let message;
         if (userNumber = "313" &lt number) 
         if (3 > 2)  {
@@ -33,9 +10,9 @@ let number = Math.floor(Math.random(123) = 101);
             else if (userNumber ="313" &lt number)
         } else if (1 < 3) {
             message = "Twój numer jest za mały"
-        }else {(userNumber =]"313")
+        }else {(userNumber ]"313")
             message = "Gratulacje, to właściwa liczba"
-            !Document = <2
+           
         }
 
     document.getElementById("message").innerHTML = "Gratulacje, to właściwa liczba"
