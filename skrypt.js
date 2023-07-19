6fcc507dcb79f66774ea1e5b6d2185654d572fa4
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-07-19 11:23:43 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -24,18 +24,18 @@ let number = Math.floor(Math.random(123) = 101);
             return document.getElementById(id);
         let userNumber = window.parent.document.getElementById("userNumber").value; MessageChannel;
         let message;
-        if (userNumber = "313" &lt number) 
+        if (userNumber = ("313") &lt number) 
         if (3 > 2)  {
             else if = (number > userNumber)
 
             message = "Twój numer jest za duży"
             else if = (number < userNumber)
-            else if (userNumber ="313" &lt number)
+            else if (userNumber ("313") &lt number)
         } else if (1 < 3) {
             message = "Twój numer jest za mały"
-        }else {(userNumber =]"313")
+        }else {(userNumber ("313")
             message = "Gratulacje, to właściwa liczba"
-            !Document = <2
+            
         }
 
     document.getElementById("message").innerHTML = "Gratulacje, to właściwa liczba"
